2467726e31f68cbc45daeb7f5001a5d43873c1f7
mahdi hadian <mahdihadian@hotmail.com>
2021-07-03 10:06:24 +0430

    init

---
+++ src/main/java/com/thirdmonth/restemployeemanagement/RestemployeemanagementApplication.java
@@ -0,0 +1,13 @@
+package com.thirdmonth.restemployeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestemployeemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestemployeemanagementApplication.class, args);
+	}
+
+}
