996d0998669c671b074ff4634b4c4b11bf2634eb
mahdi hadian <mahdihadian@hotmail.com>
2021-07-03 10:57:44 +0430

    wrote controller, need test

--- src/main/java/com/thirdmonth/restemployeemanagement/controller/EmployeeController.java
+++ src/main/java/com/thirdmonth/restemployeemanagement/controller/EmployeeController.java
@@ -1,8 +1,47 @@
 package com.thirdmonth.restemployeemanagement.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thirdmonth.restemployeemanagement.model.Employee;
+import com.thirdmonth.restemployeemanagement.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+
+    EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+//    read all employees GET /
+//    TODO maybe need to do something about responseBody
+    @GetMapping("/")
+    public List<Employee> all(){
+        return employeeService.getAll();
+    }
+
+//    save employee POST /
+    @PostMapping
+    public Employee save(@RequestBody Employee employee){
+        return employeeService.register(employee);
+    }
+
+//    update employee PUT /
+    @PutMapping
+    public Employee update(@RequestBody Employee employee){
+        return employeeService.update(employee);
+    }
+
+//    delete employee DELETE
+//    TODO need to do sth about name/value id thing
+    @DeleteMapping
+    public Long delete(@RequestParam(name = "id", value = "id") Long id){
+        return employeeService.delete(id);
+    }
 
 }
